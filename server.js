5761ea37cd2ca1b40f242d13aab7e30d8cfeee6e
Alanis Yates <yatesalanis@gmail.com>
2021-09-26 10:01:23 -0400

    fixing babel and env error

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import "dotenv/config";
 import bodyParser from "body-parser";
 
 const app = express();
@@ -12,6 +13,10 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.post("/property", (req, res, next) => {
+  res.send({ message: "Property Route" });
+});
+
 app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
+  console.log(`Server running on port: ${PORT}...`);
 });
