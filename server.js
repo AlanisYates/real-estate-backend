95648d533b0491c65d90dc58064f44fe609e45a2
Alanis Yates <yatesalanis@gmail.com>
2021-09-25 21:26:01 -0400

    setting up express server

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(morgan("combined"));
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
